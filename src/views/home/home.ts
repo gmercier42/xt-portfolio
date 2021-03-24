637ce4a5dbb831d5a96cccea2435c80355f60263
Guillaume Mercier <mercierguillaume@outlook.com>
2021-03-24 16:22:03 -0400

    Added dynamic style management

--- src/views/home/home.ts
+++ src/views/home/home.ts
@@ -1,3 +1,20 @@
 import { Vue } from "vue-class-component";
 
-export default class Home extends Vue {}
+const NUMBER_OF_STYLES = 2;
+
+export default class Home extends Vue {
+  mounted(): void {
+    this.updateReloadCounter();
+  }
+
+  get style(): number {
+    return localStorage.reloadCounter;
+  }
+
+  updateReloadCounter(): void {
+    if (localStorage.reloadCounter) localStorage.reloadCounter++;
+    else localStorage.reloadCounter = 0;
+
+    localStorage.reloadCounter = localStorage.reloadCounter % NUMBER_OF_STYLES;
+  }
+}
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="home">
+  <div class="home" :class="{ 'home--style-1': style == 1 }">
     <div class="header">
-      <div class="name">Xavier Trudeau</div>
+      <div class="name" :class="{ 'name--style-1': style == 1 }">
+        Xavier Trudeau
+      </div>
 
-      <a class="contact" href="mailto:{{contact_email}}">Contact</a>
+      <a
+        class="contact"
+        :class="{ 'contact--style-1': style == 1 }"
+        href="mailto:{{contact_email}}"
+        >Contact</a
+      >
     </div>
 
-    <div class="content">
+    <div class="content" :class="{ 'content--style-1': style == 1 }">
       <span>Graphic/Type Design</span>
       <span>(Montréal, QC, Can)</span>
     </div>
