55b2da5e9290772ba770d74deb7c0aa5254d3d11
Guillaume Mercier <mercierguillaume@outlook.com>
2021-03-24 15:08:21 -0400

    Added basic html + css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./app.vue";
 import router from "./router";
+import "simple-css-reset/reset.css";
 
 createApp(App).use(router).mount("#app");
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Hello World</div>
+  <div class="home">
+    <div class="header">
+      <div class="name">Xavier Trudeau</div>
+
+      <a class="contact" href="mailto:{{contact_email}}">Contact</a>
+    </div>
+
+    <div class="content">
+      <span>Graphic/Type Design</span>
+      <span>(Montréal, QC, Can)</span>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss" src="./home.scss"></style>
